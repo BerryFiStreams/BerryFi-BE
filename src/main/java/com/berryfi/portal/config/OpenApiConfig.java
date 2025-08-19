03289aa14464d03b550f411968313899a382fde0
Shubham Bokade <sbokade17@gmail.com>
2025-08-19 21:53:14 +0530

    swagger changes

--- src/main/java/com/berryfi/portal/config/OpenApiConfig.java
+++ src/main/java/com/berryfi/portal/config/OpenApiConfig.java
@@ -5,16 +5,22 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfig {
 
+    @Value("${app.base-url}")
+    private String baseUrl;
+
     @Bean
     public OpenAPI customOpenAPI() {
         final String securitySchemeName = "bearerAuth";
         return new OpenAPI()
+                .addServersItem(new Server().url(baseUrl))
                 .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
                 .components(
                         new Components()
