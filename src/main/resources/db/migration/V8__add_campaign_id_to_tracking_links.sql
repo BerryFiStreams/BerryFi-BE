b6e3911e598f5fa2497e137277e09392fdb91901
Shubham Bokade <sbokade17@gmail.com>
2026-03-03 09:16:58 +0530

    changes added

--- src/main/resources/db/migration/V8__add_campaign_id_to_tracking_links.sql
+++
@@ -1,5 +0,0 @@
--- Add campaign_id column to tracking_links table
-ALTER TABLE tracking_links ADD COLUMN campaign_id VARCHAR(255);
-
--- Add index for campaign lookups
-CREATE INDEX idx_tracking_link_campaign ON tracking_links(campaign_id);
